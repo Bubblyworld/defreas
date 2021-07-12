8ece297c649be16306bd11c6a55580c785052042
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2021-07-12 13:05:34 +0200

    WIP - getting the languge server off the ground

--- src/main/scala/com/cair/defreas/lsp/TextDocumentService.scala
+++ src/main/scala/com/cair/defreas/lsp/TextDocumentService.scala
@@ -6,9 +6,26 @@ import org.eclipse.lsp4j.services._
 import org.eclipse.lsp4j.services.{TextDocumentService => LspTextDocumentService}
 import java.util.concurrent.CompletableFuture
 
+/** Handles updates to text documents.
+ *  https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textSynchronization
+ */
 class TextDocumentService extends LspTextDocumentService {
-  def didChange(params: DidChangeTextDocumentParams): Unit = {}
-  def didClose(params: DidCloseTextDocumentParams): Unit = {}
-  def didOpen(params: DidOpenTextDocumentParams): Unit = {}
-  def didSave(params: DidSaveTextDocumentParams): Unit = {}
+  def didChange(params: DidChangeTextDocumentParams): Unit = {
+    println(s"Changed document: ${params.textDocument.uri}")
+    println(s"         changes: ${params.contentChanges}")
+  }
+
+  def didClose(params: DidCloseTextDocumentParams): Unit = {
+    println(s"Closed document: ${params.textDocument.uri}")
+    println(s"       contents: ${params.textDocument.text}")
+  }
+
+  def didOpen(params: DidOpenTextDocumentParams): Unit = {
+    println(s"Opened document: ${params.textDocument.uri}")
+    println(s"       contents: ${params.textDocument.text}")
+  }
+
+  def didSave(params: DidSaveTextDocumentParams): Unit = {
+    println(s"Saved document: ${params.textDocument.uri}")
+  }
 }
--- src/main/scala/com/cair/defreas/lsp/WorkspaceService.scala
+++ src/main/scala/com/cair/defreas/lsp/WorkspaceService.scala
@@ -6,10 +6,22 @@ import org.eclipse.lsp4j.services._
 import org.eclipse.lsp4j.services.{WorkspaceService => LspWorkspaceService}
 import java.util.concurrent.CompletableFuture
 
+/** Handles document workspace configuration and updates.
+ *  https://microsoft.github.io/language-server-protocol/specifications/specification-current/#workspace
+ */
 class WorkspaceService extends LspWorkspaceService {
+  /** Sent by client when configuration options are changed. We may make use
+   *  of this in the future, but for now we don't need any configuration
+   *  options.
+   */
   def didChangeConfiguration(params: DidChangeConfigurationParams): Unit = {
+    println(s"Changed configuration: ${params.settings}")
   }
 
+  /** Sent by client when there are changes to the files watched by the client.
+   *  This is irrelevant for our web-based reasoner interface.
+   */
   def didChangeWatchedFiles(params: DidChangeWatchedFilesParams): Unit = {
+    println(s"Changed watched files: ${params.changes}")
   }
 }
