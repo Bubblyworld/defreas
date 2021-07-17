e5d52e54f1020b3ca44fdca88e9f4cf8f544b6bd
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2021-07-17 11:33:19 +0200

    Comment out LSP stuff for now, get basic web editor off the ground.

--- src/main/scala/com/cair/defreas/lsp/TextDocumentService.scala
+++ src/main/scala/com/cair/defreas/lsp/TextDocumentService.scala
@@ -9,23 +9,23 @@ import java.util.concurrent.CompletableFuture
 /** Handles updates to text documents.
  *  https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textSynchronization
  */
-class TextDocumentService extends LspTextDocumentService {
-  def didChange(params: DidChangeTextDocumentParams): Unit = {
-    println(s"Changed document: ${params.textDocument.uri}")
-    println(s"         changes: ${params.contentChanges}")
-  }
-
-  def didClose(params: DidCloseTextDocumentParams): Unit = {
-    println(s"Closed document: ${params.textDocument.uri}")
-    println(s"       contents: ${params.textDocument.text}")
-  }
-
-  def didOpen(params: DidOpenTextDocumentParams): Unit = {
-    println(s"Opened document: ${params.textDocument.uri}")
-    println(s"       contents: ${params.textDocument.text}")
-  }
-
-  def didSave(params: DidSaveTextDocumentParams): Unit = {
-    println(s"Saved document: ${params.textDocument.uri}")
-  }
-}
+//class TextDocumentService extends LspTextDocumentService {
+//  def didChange(params: DidChangeTextDocumentParams): Unit = {
+//    println(s"Changed document: ${params.textDocument.uri}")
+//    println(s"         changes: ${params.contentChanges}")
+//  }
+//
+//  def didClose(params: DidCloseTextDocumentParams): Unit = {
+//    println(s"Closed document: ${params.textDocument.uri}")
+//    println(s"       contents: ${params.textDocument.text}")
+//  }
+//
+//  def didOpen(params: DidOpenTextDocumentParams): Unit = {
+//    println(s"Opened document: ${params.textDocument.uri}")
+//    println(s"       contents: ${params.textDocument.text}")
+//  }
+//
+//  def didSave(params: DidSaveTextDocumentParams): Unit = {
+//    println(s"Saved document: ${params.textDocument.uri}")
+//  }
+//}
